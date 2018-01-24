f6937610e1ef32c2a463eec27c01da8cba49f02d
xiahaiming <xiahaiming@bilibili.com>
2018-01-24 16:17:12 +0800

    fix bug

--- sqlite/main.c
+++ sqlite/main.c
@@ -16,7 +16,6 @@ void print_constants() {
 	printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
 }
 
-
 enum NodeType_t { NODE_INTERNAL, NODE_LEAF};
 typedef enum NodeType_t NodeType;
 
@@ -28,7 +27,7 @@ const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
 const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
 const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
-const uint32_t PARENT_POINTER_SIZE = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
 const uint32_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
 
 /*
@@ -40,7 +39,7 @@ const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_HEADE
 
 /*
  * leaf node body layout
- * */
+*/
 const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_KEY_OFFSET = 0; 
 const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
@@ -69,9 +68,6 @@ void *leaf_node_value(void *node, uint32_t cell_num) {
 void initialize_leaf_node(void *node) {
 	*leaf_node_num_cells(node) = 0;
 }
-
-
-
 
 enum ExecuteResult_t {EXECUTE_SUCCESS, EXECUTE_TABLE_FULL};
 typedef enum ExecuteResult_t ExecuteResult;
@@ -293,6 +289,7 @@ void db_close(Table *table) {
 		exit(EXIT_FAILURE);
 	}
 
+	// ??
 	for(uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
 		void *page = pager->pages[i];
 		if(page) {
@@ -411,7 +408,7 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table) {
 		return META_COMMAND_SUCCESS;
 	}else if (strcmp(imput_buffer->buffer, ".btree") == 0) {
 		printf("Tree: \n");
-		print_leaf_node(get_page(table->pager, 0));
+		printf_leaf_node(get_page(table->pager, 0));
 		return META_COMMAND_SUCCESS;
 	}else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;
