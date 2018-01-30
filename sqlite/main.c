f4424b706061adf9088b8b4ee6c1a0274d9efc59
jimmy <aiwei18@gmail.com>
2018-01-31 00:03:56 +0800

    finish chapter 12

--- sqlite/main.c
+++ sqlite/main.c
@@ -67,7 +67,9 @@ const uint32_t INTERNAL_NODE_CELL_SIZE =
 */
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET = LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_HEADER_SIZE + LEAF_NODE_NEXT_LEAF_SIZE;
 
 /*
  * leaf node body layout
@@ -87,6 +89,10 @@ void print_constants() {
 	printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
 	printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
 	printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+uint32_t *leaf_node_next_leaf(void *node) {
+	return node + LEAF_NODE_NEXT_LEAF_OFFSET;
 }
 
 uint32_t *leaf_node_num_cells(void *node) {
@@ -134,6 +140,7 @@ void initialize_leaf_node(void *node) {
 	set_node_type(node, NODE_LEAF);
 	set_node_root(node, false);
 	*leaf_node_num_cells(node) = 0;
+	*leaf_node_next_leaf(node) = 0;
 }
 
 uint32_t *internal_node_right_child(void *node) {
@@ -297,15 +304,12 @@ struct Cursor_t {
 typedef struct Cursor_t Cursor;
 
 Cursor *table_start(Table *table) {
-	Cursor *cursor = malloc(sizeof(Cursor));
-	cursor->table = table;
-	cursor->page_num = table->root_page_num;
-	cursor->cell_num = 0;
+	Cursor *cursor = table_find(table, 0);
 
-	void *root_node = get_page(table->pager, table->root_page_num);
-	uint32_t num_cells = *leaf_node_num_cells(root_node);
-	// jimmy: so leaf node got no cell ?
+	void *node = get_page(table->pager, cursor->page_num);
+	uint32_t num_cells = *leaf_node_num_cells(node);
 	cursor->end_of_table = (num_cells == 0);
+
 	return cursor;
 }
 
@@ -439,7 +443,13 @@ void cursor_advance(Cursor *cursor) {
 	cursor->cell_num += 1;
 
 	if (cursor->cell_num >= (*leaf_node_num_cells(node))) {
-		cursor->end_of_table = true;
+		uint32_t next_page_num = *leaf_node_next_leaf(node);
+		if(next_page_num == 0) {
+			cursor->end_of_table = true;
+		}else {
+			cursor->page_num = next_page_num;
+			cursor->cell_num = 0;
+		}
 	}
 }
 
@@ -481,6 +491,8 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
 	uint32_t new_page_num = get_unused_page_num(cursor->table->pager);
 	void *new_node = get_page(cursor->table->pager, new_page_num);
 	initialize_leaf_node(new_node);
+	*leaf_node_next_leaf(new_node) = *leaf_node_next_leaf(old_node);
+	*leaf_node_next_leaf(old_node) = new_page_num;
 
 	for (int32_t i = LEAF_NODE_MAX_CELLS; i >= 0; i--) {
 		void *destination_node;
@@ -492,9 +504,10 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
 
 		uint32_t index_within_node = i % LEAF_NODE_LEFT_SPLIT_COUNT;
 		void *destination = leaf_node_cell(destination_node, index_within_node);
-
 		if (i == cursor->cell_num) {
-			serialize_row(value, destination);
+			*leaf_node_key(destination_node, index_within_node) = key;
+			serialize_row(value, 
+					leaf_node_value(destination_node, index_within_node));
 		}else if(i > cursor->cell_num) {
 			memcpy(destination, leaf_node_cell(old_node, i - 1), LEAF_NODE_CELL_SIZE);
 		}else {
@@ -513,9 +526,7 @@ void leaf_node_split_and_insert(Cursor *cursor, uint32_t key, Row *value) {
 }
 
 void leaf_node_insert(Cursor *cursor, uint32_t key, Row *value) {
-
 	void *node = get_page(cursor->table->pager, cursor->page_num);
-
 	uint32_t num_cells = *leaf_node_num_cells(node);
 
 	if(num_cells >= LEAF_NODE_MAX_CELLS) {
