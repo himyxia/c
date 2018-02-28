9210f09789304e2e8ffd4bcb4f46aac85cef947d
jimmy <aiwei18@gmail.com>
2018-02-28 18:51:41 +0800

    ds

--- web_server/concurrent_server/seq_serv.c
+++ web_server/concurrent_server/seq_serv.c
@@ -1,12 +1,12 @@
 #include <stdint.h>
-#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <netdb.h>
-#include "util.h"
+
+#include "utils.h"
 
 
 typedef enum {WAIT_FOR_MSG, IN_MSG} ProcessingState;
